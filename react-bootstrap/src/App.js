9b1fa84659300032e63949dcc486232d45b4c35b
aniyagnik <ani789yagnik@gmail.com>
2020-08-25 19:13:33 +0530

    states and props

--- react-bootstrap/src/App.js
+++ react-bootstrap/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Navbar, NavbarBrand }from 'reactstrap'
 import "./App.css";
+import { DISHES } from './shared/dishes';
+import Menu from './components/MenuComponent';
 
 class App extends React.Component {
   constructor(props) {
     super(props); 
+    this.state = {
+      dishes:DISHES
+    }
   }
   render() {
     return (
@@ -12,8 +17,9 @@ class App extends React.Component {
         <Navbar dark color='primary'>
           <div className='container'>
             <NavbarBrand href='/'>sdf sdff fds</NavbarBrand>
-          </div>  
+          </div>
         </Navbar>
+        <Menu dishes={this.state.dishes}/>  
       </div>
     )
   }
